9f01ec934a2994999130901d3b4b6f74d1772d00
shorif <shorifpatwary999@gmail.com>
2022-11-18 11:26:38 +0600

    button and heading components

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,30 @@
 import React from "react";
-
+import { useRouter } from "next/router";
+// import Footer from "../components/Footer";
 const PageNotFound = () => {
+	const router = useRouter();
+	const goHomePage = () => {
+		router.replace("/");
+	};
 	return (
-		<div>
-			<h2>PageNotFound Custom page </h2>
-		</div>
+		<>
+			<div className="flex__column-center gap__3">
+				<h2>Your Enter a incorrect URL . </h2>
+				<h1>Page 404</h1>
+				<button onClick={goHomePage}>Go Home Page </button>
+			</div>
+		</>
 	);
 };
 
 export default PageNotFound;
+// remove header and footer for 404 page only
+PageNotFound.getLayout = (page) => {
+	return (
+		<>
+			{page}
+			<h2>Add Footer only </h2>
+			{/* <Footer /> */}
+		</>
+	);
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
 import type { AppProps } from "next/app";
-
+import Head from "next/head";
+import "@/styles/main.scss";
 const App = ({ Component, pageProps }: AppProps) => {
-	return <Component {...pageProps} />;
+	// return <Component {...pageProps} />;
+	if (Component.getLayout) {
+		return Component.getLayout(<Component {...pageProps} />);
+	}
+	return (
+		<>
+			<Head>
+				<title>Shoriful islam next js practice website </title>
+				<meta
+					name="description"
+					content="This is a next js website by shoriful islam "
+				/>
+			</Head>
+			<h2>something </h2>
+			{/* <Header /> */}
+			<Component {...pageProps} />
+			{/* <Footer /> */}
+		</>
+	);
 };
 
 export default App;
