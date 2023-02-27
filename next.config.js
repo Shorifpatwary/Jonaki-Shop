e925c75279180d343210684c65b09e798d07315b
shorif <shorifpatwary999@gmail.com>
2023-02-27 13:22:52 +0600

    [purgecss] purgecss setup for removing unused css.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const purgecss = require("@fullhuman/postcss-purgecss")({
+	content: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+	defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
+module.exports = {
 	reactStrictMode: true,
 	swcMinify: true,
 	images: {
@@ -11,9 +15,37 @@ const nextConfig = {
 		],
 	},
 	typescript: {
-		// !! WARN !!
 		ignoreBuildErrors: true,
 	},
-};
+	webpack5: (config, { isServer }) => {
+		if (!isServer) {
+			config.node = {
+				fs: "empty",
+			};
+		}
 
-module.exports = nextConfig;
+		config.module.rules.push({
+			test: /\.scss$/,
+			use: [
+				"style-loader",
+				"css-loader",
+				{
+					loader: "postcss-loader",
+					options: {
+						postcssOptions: {
+							plugins: [
+								require("postcss-import"),
+								require("tailwindcss"),
+								require("autoprefixer"),
+								...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+							],
+						},
+					},
+				},
+				"sass-loader",
+			],
+		});
+
+		return config;
+	},
+};
